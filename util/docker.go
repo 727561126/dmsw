ea30cf9fd802170142cd9ef3233f0fddec47d586
727561126 <727561126@qq.com>
2018-12-08 17:28:32 +0800

    初始化docker常用命令

--- util/docker.go
+++ util/docker.go
@@ -1,17 +1,18 @@
 package main
 
 import (
-    "os"
+	"os"
 
-    "github.com/docker/docker/client"
-    "github.com/docker/docker/api/types"
-    "github.com/docker/docker/api/types/container"
-    "github.com/docker/docker/pkg/stdcopy"
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/client"
+	"github.com/docker/docker/pkg/stdcopy"
 
-    "golang.org/x/net/context"
+	"golang.org/x/net/context"
 )
+
 type DockerAction interface {
-	get_docker()
+	get_docker() (*client.Client, error)
 	docker_start()
 	docker_stop()
 	docker_running_list()
@@ -20,56 +21,55 @@ type DockerAction interface {
 	docker_images_pull()
 	docker_images_push()
 	docker_logs()
-	
 }
 
 type MyDocker struct {
-
 }
 
-func (mydocker MyDocker) get_docker() {
-
-
+func (mydocker MyDocker) get_docker() (*client.Client, error) {
+	cli, err := client.NewClientWithOpts(client.WithVersion("1.37"))
+	if err != nil {
+		panic(err)
+	}
+	return cli, err
 }
 
 func main() {
-    ctx := context.Background()
-    cli, err := client.NewClientWithOpts(client.WithVersion("1.37"))
-    if err != nil {
-        panic(err)
-    }
+	ctx := context.Background()
+	cli, err := new(MyDocker).get_docker()
+	if err != nil {
+		panic(err)
+	}
+	_, err = cli.ImagePull(ctx, "docker.io/library/alpine", types.ImagePullOptions{})
+	if err != nil {
+		panic(err)
+	}
 
-    _, err = cli.ImagePull(ctx, "docker.io/library/alpine", types.ImagePullOptions{})
-    if err != nil {
-        panic(err)
-    }
+	resp, err := cli.ContainerCreate(ctx, &container.Config{
+		Image: "alpine",
+		Cmd:   []string{"echo", "hello world"},
+	}, nil, nil, "")
+	if err != nil {
+		panic(err)
+	}
 
-    resp, err := cli.ContainerCreate(ctx, &container.Config{
-        Image: "alpine",
-        Cmd:   []string{"echo", "hello world"},
-    }, nil, nil, "")
-    if err != nil {
-        panic(err)
-    }
+	if err := cli.ContainerStart(ctx, resp.ID, types.ContainerStartOptions{}); err != nil {
+		panic(err)
+	}
 
-    if err := cli.ContainerStart(ctx, resp.ID, types.ContainerStartOptions{}); err != nil {
-        panic(err)
-    }
+	statusCh, errCh := cli.ContainerWait(ctx, resp.ID, container.WaitConditionNotRunning)
+	select {
+	case err := <-errCh:
+		if err != nil {
+			panic(err)
+		}
+	case <-statusCh:
+	}
 
-    statusCh, errCh := cli.ContainerWait(ctx, resp.ID, container.WaitConditionNotRunning)
-    select {
-    case err := <-errCh:
-        if err != nil {
-            panic(err)
-        }
-    case <-statusCh:
-    }
+	out, err := cli.ContainerLogs(ctx, resp.ID, types.ContainerLogsOptions{ShowStdout: true})
+	if err != nil {
+		panic(err)
+	}
 
-    out, err := cli.ContainerLogs(ctx, resp.ID, types.ContainerLogsOptions{ShowStdout: true})
-    if err != nil {
-        panic(err)
-    }
-
-    stdcopy.StdCopy(os.Stdout, os.Stderr, out)
+	stdcopy.StdCopy(os.Stdout, os.Stderr, out)
 }
-
